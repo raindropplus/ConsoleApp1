372457c2445b24244b6f30151c408c9700ca5bfc
Md.Humayun Kabir <raindropplus11@gmail.com>
2020-12-25 07:05:53 -0800

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Process[] processes = Process.GetProcesses();
+            Process[] processes = Process.GetProcesses();//get prc
             foreach (var proc in processes)
             {
                 if (!string.IsNullOrEmpty(proc.MainWindowTitle))
