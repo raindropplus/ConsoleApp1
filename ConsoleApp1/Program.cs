b3f3c12f7f5a0a4aa06f7a8b3f8538c35da79c8f
RONY <RONY@RONY>
2020-12-25 06:31:26 -0800

    Show runnig app

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Process[] processes = Process.GetProcesses();
+            foreach (var proc in processes)
+            {
+                if (!string.IsNullOrEmpty(proc.MainWindowTitle))
+                    Console.WriteLine(proc.ProcessName);
+                if (proc.ProcessName.ToUpper() == "NOTEPAD")
+                {
+                    proc.Kill();
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
